faaf253ffa6be1848b124a91fcc2339eb0860b75
Fabio Kusaba <fabinhokb44@gmail.com>
2025-05-26 20:20:14 -0300

    refactor: eliminar anotações Lombok e habilitar auditoria da entidade

--- backend/src/main/java/br/edu/toledoprudente/fabiokusaba/myfinance/models/Finance.java
+++ backend/src/main/java/br/edu/toledoprudente/fabiokusaba/myfinance/models/Finance.java
@@ -2,22 +2,16 @@ package br.edu.toledoprudente.fabiokusaba.myfinance.models;
 
 import br.edu.toledoprudente.fabiokusaba.myfinance.enums.FinanceType;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "tb_finance")
-@Getter
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
 public class Finance {
 
     @Id
@@ -44,4 +38,71 @@ public class Finance {
     @LastModifiedDate
     @Column(name = "updated_at")
     private LocalDateTime updatedAt;
+
+    public Finance() {
+    }
+
+    public Finance(Long id, String nome, String descricao, BigDecimal valor, FinanceType tipo) {
+        this.id = id;
+        this.nome = nome;
+        this.descricao = descricao;
+        this.valor = valor;
+        this.tipo = tipo;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public BigDecimal getValor() {
+        return valor;
+    }
+
+    public void setValor(BigDecimal valor) {
+        this.valor = valor;
+    }
+
+    public FinanceType getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(FinanceType tipo) {
+        this.tipo = tipo;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
 }
